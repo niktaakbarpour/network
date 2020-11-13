28b2d6abf79ebf309dcc7f164d586f5c8dd4312a
nikta <nikta.akbarpour99gmail.com>
2020-11-14 00:15:40 +0330

    table

---
+++ src/component/Page/Page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NestedList from "../SearchBox/NestedList";
+import BasicTable from "../BasicTable/BasicTable";
+
+function Page(){
+    return(
+    <div>
+        <NestedList />
+        <BasicTable />
+    </div>
+    );
+}
+
+export default Page;
