04f454aeed94da49bc6be310c3c1367a3752978e
nikta <nikta.akbarpour99gmail.com>
2020-11-25 23:48:15 +0330

    border for interfaces

--- src/component/interfaces/Interfaces.js
+++ src/component/interfaces/Interfaces.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import React, {useEffect} from 'react';
+import {createMuiTheme, makeStyles, ThemeProvider} from '@material-ui/core/styles';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -7,41 +7,52 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import SendIcon from '@material-ui/icons/Send';
 import axios from "axios";
-import Spinner from "./spinner/Spinner";
 
 const useStyles = makeStyles((theme) => ({
         root: {
-            // width: '70%',
             maxWidth: '70%',
-            backgroundColor: theme.palette.background.paper,
-            margin: "auto"
+            margin: "auto",
+            border: "5px solid #4caf50",
+            borderRadius: "5px",
+            backgroundColor: "#f1f8e9",
+            marginTop: "5px"
         },
         nested: {
-            paddingLeft: theme.spacing(4)
-        }
+            paddingLeft: theme.spacing(4),
+        },
     })
 );
+
+const theme = createMuiTheme({
+    overrides: {
+        MuiListSubheader: {
+            root: {
+                paddingTop: "5px"
+            }
+        },
+    }
+});
 
 export default function Interfaces(props) {
     const classes = useStyles();
     const [interfaces, setInterfaces] = React.useState([]);
-    const [loading, setLoading] = useState(true);
+    // const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios.get("/interface")
             .then(res => {
                 setInterfaces(res.data)
-                setLoading(false);
+                // setLoading(false);
             })
     }, [])
 
     const handleClick = (name) => {
-        setLoading(true);
+        // setLoading(true);
         axios.post(
             "/interface",
             {name: name}
         ).then(res => {
-            setLoading(false);
+            // setLoading(false);
             if (res.status === 200) {
                 props.history.push("/EachInterface");
             }
@@ -50,30 +61,32 @@ export default function Interfaces(props) {
 
     return (
         <div>
-            {loading ? <Spinner/> : null}
-            <List
-                component="nav"
-                aria-labelledby="nested-list-subheader"
-                subheader={
-                    <ListSubheader component="div" id="nested-list-subheader">
-                        Network Interfaces
-                    </ListSubheader>
-                }
-                className={classes.root}
-            >
-                {
-                    interfaces.map(value => {
-                        return (
-                            <ListItem key={value.id} button onClick={handleClick.bind(null, value.name)}>
-                                <ListItemIcon>
-                                    <SendIcon/>
-                                </ListItemIcon>
-                                <ListItemText primary={value.name}/>
-                            </ListItem>
-                        )
-                    })
-                }
-            </List>
+            {/*{loading ? <Spinner/> : null}*/}
+            <ThemeProvider theme={theme}>
+                <List
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                    subheader={
+                        <ListSubheader component="div" id="nested-list-subheader">
+                            Network Interfaces
+                        </ListSubheader>
+                    }
+                    className={classes.root}
+                >
+                    {
+                        interfaces.map(value => {
+                            return (
+                                <ListItem key={value.id} button onClick={handleClick.bind(null, value.name)}>
+                                    <ListItemIcon>
+                                        <SendIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary={value.name}/>
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
+            </ThemeProvider>
         </div>
     );
 }
